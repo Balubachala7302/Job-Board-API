7248a855e00b93ac5283e87fc7f707c7ed85a58c
Bachala Bala Bhaskar <208r1a0507.cmrec@gmail.com>
2026-03-16 22:52:08 +0530

    Day-4

---
+++ app/db/schemas.py
@@ -0,0 +1,56 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class UserBase(BaseModel):
+    name:str
+    email:str
+
+class UserCreate(BaseModel):
+    password:str
+
+class UserResponse(BaseModel):
+    id:str
+    role:str
+
+    class Config:
+        from_attributes=True
+
+class CompanyCreate(BaseModel):
+    name:str
+    description:Optional[str]=None
+
+class CompanyResponse(BaseModel):
+    id:int
+    name:str
+    description:Optional[str]
+
+    class Config:
+        from_atttributes=True
+
+class JobCreate(BaseModel):
+    title:str
+    description:str
+    location:str
+
+class JobResponse(BaseModel):
+    id:int
+    title:str
+    description:str
+    location:str
+
+    class Config:
+        from_attributes=True
+
+class ApplicationCreate(BaseModel):
+    job_id:int
+
+class ApplicationResponse(BaseModel):
+    id:int
+    user_id:int
+    job_id:int
+    status:int
+
+    class Config:
+        from_attributes=True
+
+
